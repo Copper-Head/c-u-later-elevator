38e243be61ed3fcd2e3abb5c8f784219936d119b
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-03-25 14:30:01 +0100

    revising elevator logic from scratch

--- getPlan.sh
+++ getPlan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 encoding="elevator.lp"
+end=2
 
 if [ -z "$1" ]
   then
@@ -9,7 +10,8 @@ if [ -z "$1" ]
     if [ -n "$2" ]
     then
       encoding="$2"
+      end=3
     fi
-    clingo $1 $encoding --outf=1 --quiet=1,0 | grep -A1 ANSWER | tail -n1 | tr " " "\n"
+    clingo $1 $encoding --outf=1 --quiet=1,0 ${@:$end} | grep -A1 ANSWER | tail -n1 | tr " " "\n"
 
 fi
