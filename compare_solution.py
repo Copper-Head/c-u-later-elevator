b67a6652d1aaa6c0705218673e9eeb15e7936d21
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-03-28 23:14:57 +0200

    Adds ability to send STDIN as something to compare a solution with

--- compare_solution.py
+++ compare_solution.py
@@ -1,7 +1,7 @@
 import argparse
 import fileinput
 import sys
-
+import re
 """
 Hacky script for comparing output set to gold set.
 
@@ -15,7 +15,8 @@ Usage:
 def compare_sets(s1, s2):
     """Compare the sets."""
     if len(s1) != 0:
-        return s1 == s2
+        # return s1 == s2
+        return s1 - s2
     return False
 
 
@@ -29,23 +30,50 @@ def read_from_stdin():
     return s1
 
 
+def order_output(s):
+    # 'do(elevator(2),serve,62).'
+    # t = ('do(elevator(2),serve,62).', 62)
+    modified_set = []
+    for i in s:
+        time_step = int(re.search("(\d{1,4})\)\.", i).group(1))
+        modified_set.append((i, time_step))
+    # return sorted(modified_set, key=lambda x: x[1])
+    return [i[0] for i in sorted(modified_set, key=lambda x: x[1])]
+
+
+def file2set(file_obj):
+    """Collect elements from solution in set."""
+    return set(line.strip() for line in file_obj)
+
+
 def read_from_file(file_name):
     """Collect elements from solution in set."""
     s2 = set()
     with open(file_name, "r") as f:
-        for line in f:
-            s2.add(line.strip())
-    return s2
+        return file2set(f)
 
 
 if __name__ == "__main__":
 
-    print(sys.argv[1])
-    print(sys.argv[2])
+    prs = argparse.ArgumentParser()
+    prs.add_argument('expected')
+    prs.add_argument('ours', nargs="?")
+    args = prs.parse_args()
 
-    s1 = read_from_file(sys.argv[1])
-    print(s1)
-    s2 = read_from_file(sys.argv[2])
-    print(s2)
+    expected_set = read_from_file(args.expected)
+    if args.ours:
+        our_set = read_from_file(args.ours)
+    else:
+        our_set = file2set(sys.stdin)
 
-    print("\ncorrect solution: {}\n".format(compare_sets(s1, s2)))
+    # print("\ncorrect solution: {}\n".format(compare_sets(test, gold)))
+    # print("\ndifferences in set 1 and set 2:\n\n {}\n".format(compare_sets(test, gold)))
+
+    test_ordered = order_output(our_set - expected_set)
+    gold_ordered = order_output(expected_set - our_set)
+
+    with open("our-output.lp", "w") as f:
+        f.write("\n".join(test_ordered))
+
+    with open("expected-output.lp", "w") as f:
+        f.write("\n".join(gold_ordered))
